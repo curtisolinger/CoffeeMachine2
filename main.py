78f5b50367367b2476eb3758791e65589e3897c1
Curtis Olinger <curtisolinger@gmail.com>
2023-02-02 14:46:11 -0800

    Streamlined main.py and added a Sublime Text project file

--- main.py
+++ main.py
@@ -7,22 +7,15 @@ coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
 off = False
+
 while not off:
-	response = input(f"What would you like? ({menu.get_items()}): ")
-
-	if response == "off":
-            off = True
-	elif response == "report":
-		coffee_maker.report()
-		money_machine.report()
-	else:
-		drink = menu.find_drink(response)
-
-		if coffee_maker.is_resource_sufficient(drink):
-			if money_machine.make_payment(drink.cost):
-				coffee_maker.make_coffee(drink)
-			else:
-				print("Sorry that's not enough money. Money refunded.")
-
-		else:
-			print("Sorry there is not enough water")
+    response = input(f"What would you like? ({menu.get_items()}): ")
+    if response == "off":
+        off = True
+    elif response == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(response)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
